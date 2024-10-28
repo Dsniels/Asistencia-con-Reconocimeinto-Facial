783d9eac0d66e8b7a9a3413dae1ddae14d894b3d
Daniel Salazar <dsnile.salazr@gmail.com>
2024-10-28 17:59:12 -0600

    Refactorizar PrepareData.ts a PhotosActions.ts y agregar función prepareRegister

--- Service/PhotosActions.ts
+++ Service/PhotosActions.ts
@@ -1,20 +1,15 @@
 import { CameraView } from "expo-camera";
-import { Kairos } from "./Api/Actions";
+import { Api } from "./Api/Actions";
 import { CameraCapturedPicture } from "expo-camera/build/legacy/Camera.types";
+import { Alumno } from "@/Types/Registro";
 
-export const snap = async (condition: boolean, camera: CameraView) => {
+export const snap = async (camera: CameraView) => {
 	try {
 		if (camera) {
 			let photo = await camera.takePictureAsync({ base64: true });
 			if (photo) {
 				const data = prepareData(photo);
-
-				condition
-					? Kairos.reconocimiento(data)
-					: Kairos.registro({
-							userId: "Nuevo Usuario",
-							imagen: data,
-					  });
+				 Api.reconocimiento(data)
 			}
 		}
 	} catch (e) {
@@ -25,11 +20,19 @@ export const snap = async (condition: boolean, camera: CameraView) => {
 const prepareData = (imagen: CameraCapturedPicture): FormData => {
 	const data = new FormData();
 	let filename = imagen.uri.split("/").pop();
-
 	let match = /\.(\w+)$/.exec(filename as string);
 	let type = match ? `image/${match[1]}` : `image`;
     //@ts-ignore
 	data.append("file", { uri: imagen.uri, name: "file", type });
-
 	return data;
 };
+
+export const prepareRegister = (usuario : Alumno) =>{
+	const value = Object.values(usuario)
+	const valueFiltrados = value.filter(value => typeof value ==='string'  )
+	const nombre = valueFiltrados.join(' ').toUpperCase() + usuario.matricula;
+	let formData = prepareData(usuario.imagen as CameraCapturedPicture)
+	formData.append('nombre',nombre);
+	Api.registro(formData)
+
+}
